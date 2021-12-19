9f009b84ec5d288c294e3c4358486910558c3d56
Yasin Guelay <62594737+yasinguelay@users.noreply.github.com>
2021-12-19 11:11:29 +0100

    Fixed Typos

--- server/routes/filme.js
+++ server/routes/filme.js
@@ -3,8 +3,8 @@ const fetch = require('node-fetch');
 const dbo = require('../db/conn');
 const movies = express.Router();
 
-
-movies.route('/filme')
+movies
+  .route('/filme')
   .get(function (_req, res) {
     const dbConnect = dbo.getDb();
 
@@ -13,7 +13,7 @@ movies.route('/filme')
       .find({})
       .toArray(function (err, result) {
         if (err) {
-          res.status(400).send('Error fetching movies!');
+          res.status(400).send('Fehler beim Abrufen der Filme!');
         } else {
           res.json(result);
         }
@@ -21,50 +21,57 @@ movies.route('/filme')
   })
   .post(async function (req, res) {
     const dbConnect = dbo.getDb();
-    const newMovieToInsert = {
-      titel: req.body.titel
-    };
+    const newMovieToInsert = { titel: req.body.titel };
 
-    const responseImdbSearchMovie = await fetch('https://imdb-api.com/en/API/SearchMovie/' + process.env.IMDB_API + '/' + newMovieToInsert.titel);
+    const responseImdbSearchMovie = await fetch(
+      'https://imdb-api.com/en/API/SearchMovie/' +
+        process.env.IMDB_API +
+        '/' +
+        newMovieToInsert.titel
+    );
     const resultsImdbSearchMovie = await responseImdbSearchMovie.json();
 
-    const responseImdbTitle = await fetch('https://imdb-api.com/de/API/Title/' + process.env.IMDB_API + '/' + resultsImdbSearchMovie.results[0].id);
+    const responseImdbTitle = await fetch(
+      'https://imdb-api.com/de/API/Title/' +
+        process.env.IMDB_API +
+        '/' +
+        resultsImdbSearchMovie.results[0].id
+    );
     const resultsImdbTitle = await responseImdbTitle.json();
-
 
     newMovieToInsert.bild = resultsImdbTitle.image;
     newMovieToInsert.dauer = parseInt(resultsImdbTitle.runtimeMins);
 
     dbConnect
       .collection('film')
-      .insertOne(newMovieToInsert, function (err, result) {
+      .insertOne(newMovieToInsert, function (err, _result) {
         if (err) {
-          res.status(400).send('Error inserting movie!');
+          res
+            .status(400)
+            .send(`"${newMovieToInsert.titel}" wurde bereits angelegt!`);
         } else {
-          console.log(`Added a new movie with id ${result.insertedId}`);
+          console.log(`"${newMovieToInsert.titel}" wurde angelegt.`);
           res.status(201).send();
         }
       });
   });
 
-movies.route('/filme/:titel')
-  .delete((req, res) => {
-    const dbConnect = dbo.getDb();
-    const movieToDelete = { titel: req.params.titel };
+movies.route('/filme/:titel').delete((req, res) => {
+  const dbConnect = dbo.getDb();
+  const movieToDelete = { titel: req.params.titel };
 
-    dbConnect
-      .collection('film')
-      .deleteOne(movieToDelete, function (err, _result) {
-        if (err) {
-          res
-            .status(400)
-            .send(`Error deleting movie with name ${req.params.titel}!`);
-        } else {
-          console.log('Movie deleted');
-          res.status(204).send();
-        }
-      });
-  });
-
+  dbConnect
+    .collection('film')
+    .deleteOne(movieToDelete, function (err, _result) {
+      if (err) {
+        res
+          .status(400)
+          .send(`Fehler beim Entfernen von "${movieToDelete.titel}"!`);
+      } else {
+        console.log(`"${movieToDelete.titel}" wurde entfernt.`);
+        res.status(204).send();
+      }
+    });
+});
 
 module.exports = movies;
--- server/routes/saele.js
+++ server/routes/saele.js
@@ -2,53 +2,54 @@ const express = require('express');
 const dbo = require('../db/conn');
 const halls = express.Router();
 
+halls.route('/saele').post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const newHallToInsert = {
+    nummer: req.body.nummer,
+    sitzplaetze: req.body.sitzplaetze,
+  };
+  const uniques = new Set(
+    newHallToInsert.sitzplaetze.map((item) =>
+      (item.reihe + item.nummer).toLowerCase()
+    )
+  );
 
-halls.route('/saele')
-  .post(function (req, res) {
-    const dbConnect = dbo.getDb();
-    const newHallToInsert = { 
-      nummer: req.body.nummer,
-      sitzplaetze: req.body.sitzplaetze
-    };
-    const uniques = new Set(newHallToInsert.sitzplaetze.map(item => (item.reihe + item.nummer).toLowerCase()));
-
-    if ([...uniques].length === newHallToInsert.sitzplaetze.length) {
-      dbConnect
-        .collection('saal')
-        .insertOne(newHallToInsert, function (err, result) {
-          if (err) {
-            res.status(400).send('Hall already exists!');
-          } else {
-            console.log(`Added a new hall with number ${newHallToInsert.nummer}`);
-            res.status(201).send();
-          }
-        });
-    } else {
-      res.status(400).send('Error: Same seat inserted twice!');
-    } 
-
-  });
-
-halls.route('/saele/:nummer')
-  .delete((req, res) => {
-    const dbConnect = dbo.getDb();
-    const hallToDelete = {
-      nummer: parseInt(req.params.nummer)
-    };
-
+  if ([...uniques].length === newHallToInsert.sitzplaetze.length) {
     dbConnect
       .collection('saal')
-      .deleteOne(hallToDelete, function (err, _result) {
+      .insertOne(newHallToInsert, function (err, _result) {
         if (err) {
           res
             .status(400)
-            .send(`Error deleting hall with number ${req.params.nummer}!`);
+            .send(`Saal ${newHallToInsert.nummer} wurde bereits angelegt.`);
         } else {
-          console.log('Hall deleted');
-          res.status(204).send();
+          console.log(`Saal ${newHallToInsert.nummer} wurde angelegt.`);
+          res.status(201).send();
         }
       });
-  });
+  } else {
+    res
+      .status(400)
+      .send(
+        `Saal ${newHallToInsert.nummer} konnte nicht angelegt werden. Selber Sitz wurde mehrfach eingefuegt.`
+      );
+  }
+});
 
+halls.route('/saele/:nummer').delete((req, res) => {
+  const dbConnect = dbo.getDb();
+  const hallToDelete = { nummer: parseInt(req.params.nummer) };
+
+  dbConnect.collection('saal').deleteOne(hallToDelete, function (err, _result) {
+    if (err) {
+      res
+        .status(400)
+        .send(`Saal ${hallToDelete.nummer} konnte nicht entfernt werden!`);
+    } else {
+      console.log(`Saal ${hallToDelete.nummer} wurde entfernt.`);
+      res.status(204).send();
+    }
+  });
+});
 
 module.exports = halls;
