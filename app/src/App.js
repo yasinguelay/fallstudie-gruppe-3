2133dfbe165b31aab2446aee7095b7eb9e99be26
Yasin Guelay <62594737+yasinguelay@users.noreply.github.com>
2022-01-03 16:23:01 +0100

    Added Login/Logout Tooltip

--- app/src/App.js
+++ app/src/App.js
@@ -19,6 +19,7 @@ import Slider from 'react-slick';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import SeatsModal from './SeatsModal.js';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 function App() {
   const { isLoading, isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
@@ -472,19 +473,31 @@ function App() {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           {isAuthenticated ? (
-            <Navbar.Brand href="" style={{ flexBasis: 0 }} className="flex-grow-1 order-lg-last text-end m-0" onClick={() => logout({returnTo: window.location.origin})}>
-             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-box-arrow-left" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
-                <path fillRule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
-              </svg>
-            </Navbar.Brand>
+              <Row style={{ flexBasis: 0, cursor: 'pointer' }} className="flex-grow-1 order-lg-last text-end m-0 justify-content-end">
+                <OverlayTrigger placement="bottom" delay={{ show: 250, hide: 400 }} overlay={<Tooltip id="logout-tooltip">Logout</Tooltip>}>
+                  <Col xs='auto' className='pe-0'>
+                    <Navbar.Brand href="" onClick={() => logout({returnTo: window.location.origin})}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
+                        <path fillRule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
+                      </svg>
+                    </Navbar.Brand>
+                  </Col>
+                </OverlayTrigger>
+              </Row>
           ) : (
-            <Navbar.Brand href="" style={{ flexBasis: 0 }} className="flex-grow-1 order-lg-last text-end m-0" onClick={() => loginWithRedirect()}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-              </svg>
-            </Navbar.Brand>
+              <Row style={{ flexBasis: 0, cursor: 'pointer' }} className="flex-grow-1 order-lg-last text-end m-0 justify-content-end">
+                <OverlayTrigger placement="bottom" delay={{ show: 250, hide: 400 }} overlay={<Tooltip id="login-tooltip">Login</Tooltip>}>
+                  <Col xs='auto' className='pe-0'>
+                    <Navbar.Brand href="" style={{ flexBasis: 0, cursor: 'pointer' }} className="flex-grow-1 order-lg-last text-end m-0" onClick={() => loginWithRedirect()}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                      </svg>
+                    </Navbar.Brand>
+                  </Col>
+                </OverlayTrigger>
+              </Row>
           )}
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav style={{ flexBasis: 0 }} className="me-auto flex-grow-1 justify-content-center">
